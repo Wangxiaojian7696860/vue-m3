723a0538d3b706cd81ae5eb2b2d16b178d6f34c1
Wangxiaojian7696860 <32383683+Wangxiaojian7696860@users.noreply.github.com>
2019-08-10 17:35:27 +0800

    Add files via upload

---
+++ src/components/disc/disc.vue
@@ -0,0 +1,70 @@
+<template>
+   <transition name="slide">
+      <music-list  :songs="songs" :title="title" :bgImage="bgImage"></music-list>
+   </transition>
+</template>
+<script>
+  import MusicList from '../music-list/music-list'
+  import { mapGetters } from 'vuex'
+  import {song} from '../../api/api'
+  export default{
+    data(){
+	   return {
+	      songs:[]
+	   }
+	},
+	methods:{
+		_getSongList(){
+			if (!this.getDisc.dissid) {
+			  this.$router.push('/recommend')
+			  return
+			}
+			this.$http({
+				method: 'get',
+				url:"https://wangxiaojian7696860.github.io/cao/static/data/singerdetail.json"
+			}).then((res)=>{
+                if(res.status ==200){
+				      for(var item in res.data){
+					     if(item == this.getDisc.dissid){
+						   this.songs = this._normalizeSongs(res.data[item].list);		
+						 }
+					  }					
+				}
+			})			
+		},
+		_normalizeSongs(data){
+			var ret = [];
+           data.forEach(item => {
+			   ret.push(new song(item.musicData))
+		   });
+		   return ret;
+		}
+	},
+	created(){
+       this._getSongList();
+	},
+	components:{
+		MusicList
+	},
+	computed:{
+		title(){
+			return this.getDisc.name
+		},
+		bgImage(){
+			return this.getDisc.avtar
+		},
+		...mapGetters([
+			"getDisc"
+		])
+	}
+    
+  }
+</script>
+<style scoped lang="scss">
+  .slide-enter-active, .slide-leave-active{
+    transition: all 0.3s
+  }
+  .slide-enter, .slide-leave-to{
+    transform: translate3d(100%, 0, 0)
+  }	
+</style>
---
+++ src/components/music-list/music-list.vue
@@ -0,0 +1,185 @@
+<template>
+   <div class="music-list">
+ 		<div class="back" @click="back">
+		  <i class="icon-back">&lt;</i>
+		</div> 
+		<h1 class="title" v-html="title"></h1>
+		<div class="bg-image" ref="bgImage" :style="bgStyle">
+		     <div class="play-wrapper">
+			      <div ref="playBtn" class="play" @click="random" v-show="songs.length>0">
+					    <span class="text">随机播放全部</span>
+				  </div>			 
+			 </div>
+			 <div class="filter" ref="filter"></div>
+		</div>
+		<div class="bg-layer" ref="layer"></div>
+		<scroll :data="songs" :jianting="listenScroll" :probe-type="probeType" @scroll="scroll" class="list" ref="list">
+		   <div class="song-list-wrapper">
+			  <song-list :songs="songs" @select = "selectItem"></song-list>
+			  <div class="a">你妹妹</div>
+			  <div class="a">你妹妹</div>
+			  <div class="a">你妹妹</div>
+			  <div class="a">你妹妹</div>
+			  <div class="a">你妹妹</div>
+			  <div class="a">你妹妹</div>
+			  <div class="a">你妹妹</div>
+			  <div class="a">你妹妹</div>
+			  <div class="a">你妹妹</div>
+			  <div class="a">你妹妹</div>
+		   </div>		        
+		</scroll>  
+   </div>
+</template>
+<script>
+import scroll from "../../base/scroll/scroll.vue"
+import SongList from '../../base/song-list/song-list'
+//import {mapActions} from 'vuex'
+const RESERVED_HEIGHT = 40
+export default{
+    props:{
+	    title:{
+		    type:String,
+			default:''
+		},
+		bgImage:{
+		    type:String,
+			default:''		
+		},
+		songs:{
+		    type:Array,
+			default:[]
+		}
+	},
+	 data(){
+	     return{
+		    probeType:3,
+			listenScroll:true,
+			scrollY: 0
+		 }
+	 },
+	 mounted(){
+		 var imageHeight = document.querySelector('.bg-image').offsetHeight;//this.$refs.bgImage.offsetHeight
+		 this.$refs.list.$el.style.top = imageHeight+"px";
+
+
+	 },
+	 methods:{
+		 back(){
+			 this.$router.go(-1);
+		 },
+		 random(){},
+		 scroll(){},
+		 selectItem(song,index){
+
+		 }
+	 },
+	 components:{
+		 scroll,
+		 SongList
+	 },
+	 computed:{
+		 bgStyle(){
+			return 'background-image: url('+this.bgImage+');'
+		 }
+	 }
+}
+</script>
+<style lang="scss" scoped>
+@import "../../common/css/variable";
+@import "../../common/css/mixin";   
+.music-list{
+	position: fixed;
+	z-index: 100;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	background: $color-background;
+	.back{
+      position:absolute;
+      top: 0;
+      left: 6px;
+      z-index: 50;
+	  .icon-back{
+        display: block;
+        padding: 10px;
+        font-size: $font-size-large-x;
+        color: $color-theme;
+	  }		
+	
+	}
+	.title{
+      position: absolute;
+      top: 0;
+      left: 10%;
+      z-index: 40;
+      width: 80%;
+      @include no-wrap();
+      text-align: center;
+      line-height: 40px;
+      font-size: $font-size-large;
+      color: $color-text;
+	}
+	.bg-image{
+      position: relative;
+      width: 100%;
+      height: 0;
+      padding-top: 70%;
+      transform-origin: top;
+      background-size: cover;
+	  .play-wrapper{
+         position: absolute;
+         bottom: 20px;
+         width: 100%;
+		 z-index:1;
+		 .play{
+            box-sizing: border-box;
+              width: 135px;
+              padding: 7px 0;
+              margin: 0 auto;
+			  text-align: center;
+			  border: 1px solid $color-theme;
+			  color: $color-theme;
+			  border-radius: 100px;
+			  font-size: 0;	
+			  .icon-play{
+                 display: inline-block;
+                 vertical-align: middle;
+                 margin-right: 6px;
+                  font-size: $font-size-medium-x;
+		       }	
+			  .text{
+				display: inline-block;
+				vertical-align: middle;
+				font-size: $font-size-small;
+			  }		 
+		 } 
+	  }
+	  .filter{
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(7, 17, 27, 0.4);
+	  }	
+	} 
+	.list{
+      position: fixed;
+      top: 0;
+      bottom:0;
+      width: 100%;
+      background: $color-background;
+	  .song-list-wrapper{
+        padding: 20px 30px;
+		.a{line-height:40px;color:#fff}
+	  }		
+	}
+    .bg-layer{
+      position: relative;
+      height: 100%;	  
+      background: $color-background;
+	}  
+	
+}	
+</style>
