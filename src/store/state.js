fd87d1bd6cfd600e934482af64795c1048edfc4a
Wangxiaojian7696860 <32383683+Wangxiaojian7696860@users.noreply.github.com>
2019-08-12 17:37:23 +0800

    Update state.js

--- src/store/state.js
+++ src/store/state.js
@@ -1,4 +1,12 @@
+import {playMode} from '../common/js/config'
 export const state = {
     singer:{},//存当前选择的歌手信息 通过它的id可以切换到相关歌手页面
     disc:{},//相当于singer//recommend子目录 disc.vue
+    playing:false,//播放状态
+    fullScreen:false,//全屏播放
+    playList:[],//播放列表
+    sequenceList:[],//顺序播放列表因为播放可以随机播放	
+    currentIndex:-1,
+    mode:playMode.sequence,
+
 }
