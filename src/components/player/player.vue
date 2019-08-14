deee20abd9ee3b25ccf517493ca02f016f5767ea
Wangxiaojian7696860 <32383683+Wangxiaojian7696860@users.noreply.github.com>
2019-08-14 22:59:35 +0800

    Add files via upload

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -52,7 +52,7 @@
           </div>
           <div class="operators">
             <div class="icon i-left">
-              <i :class="iconMode"></i>
+              <i :class="iconMode" @click="changeMode"></i>
             </div>
             <div class="icon i-left" :class="disableCls">
               <i @click="prev" class="icon-prev"></i>
@@ -98,6 +98,7 @@ import Scroll from "../../base/scroll/scroll";
 import ProgressBar from "../../base/progress-bar/progress-bar";
 import ProgressCircle from "../../base/progress-circle/progress-circle";
 import { prefixStyle } from "../../common/js/dom";
+import {shuffle} from '../../common/js/util'
 const transform = prefixStyle("transform");
 const transitionDuration = prefixStyle("transitionDuration");
 //Lyric 的api
@@ -131,7 +132,26 @@ export default {
     back() {
       this.setFullScreen(false);
     },
-
+    changeMode(){
+	   var mode = this.getMode;
+	   mode = (mode+1)%3;
+	   this.setMode(mode);
+	   let list = null;
+	    if (mode === 2) {
+		  list = shuffle(this.getSequenceList)
+		} else {
+		   list = this.getSequenceList
+		}
+		  this.resetCurrentIndex(list)
+		  this.setPlaylist(list)	   
+	   
+	},
+	resetCurrentIndex(list){
+      let index = list.findIndex((item) => {
+        return item.id === this.currentSong.id
+      })
+      this.setCurrentIndex(index)	
+	},
     open() {
       this.setFullScreen(true);
     },
@@ -309,7 +329,9 @@ export default {
     ...mapMutations({
 	  setFullScreen: "changeFullScreen",
 	  setPlaying:"changePlaying",
-	  setCurrentIndex:"changeCurrentIndex"
+	  setCurrentIndex:"changeCurrentIndex",
+	  setMode:"changeMode",
+	  setPlaylist:"changePlayList"
     }),
     ...mapActions(["savePlayHistory"])
   },
@@ -338,9 +360,9 @@ export default {
       return this.getPlaying ? "play" : "play pause";
     },
     iconMode() {
-      return this.mode == 0
+      return this.getMode == 0
         ? "icon-sequence"
-        : this.mode == 1
+        : this.getMode == 1
         ? "icon-loop"
         : "icon-random";
     },
@@ -362,6 +384,7 @@ export default {
       "getCurrentIndex",
       "currentSong",
 	  "getPlaylist",
+	  "getSequenceList",
 	  "playHistory",
 	  "getMode"
     ])
