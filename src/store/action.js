1f67af5d060488b20031550f2130678c47414c3f
Wangxiaojian7696860 <32383683+Wangxiaojian7696860@users.noreply.github.com>
2019-08-16 17:30:46 +0800

    Add files via upload

--- src/store/action.js
+++ src/store/action.js
@@ -1,35 +1,21 @@
 import {saveSearch, savePlay, saveFavorite,deleteFavorite} from '../common/js/cache'
-import {shuffle} from '../common/js/util'
-
-function findIndex(list, song) {
-  return list.findIndex((item) => {
-    return item.id === song.id
-  })
-}
+import {shuffle} from '../../common/js/util'
 export const actions = {
     selectPlay({commit,state},{list,index}){
 		
         commit('changeSequenceList',list);
 		
 		if(state.mode==2){
-		  list = shuffle(list); 
-		  index = findIndex(list, list[index])	
+			list = shuffle(list);
+			index = list.findIndex((item) => {
+              return item.id === state.currentSong.id
+            })
 		}
 		
 		commit('changePlayList',list);
 		commit('changeCurrentIndex',index);
 		commit('changePlaying',true);
 		commit('changeFullScreen',true);
-	},
-	randomPlay({commit}, {list}){
-		commit('changeSequenceList',list);
-		commit('changeMode',2);
-		list = shuffle(list); 
-		commit('changeFullScreen',true);
-		commit('changePlaying',true);
-		commit('changeCurrentIndex',0);
-		commit('changePlayList',list);
-		 
 	},
 	savePlayHistory({commit,state},song){
 		commit('changePlayhistory', savePlay(song))
